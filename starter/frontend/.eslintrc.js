435a26e5b82b78365b9e5f1e65ce61dd316fe820
mrduyprohk1 <135818186+mrduyprohk1@users.noreply.github.com>
2024-12-07 09:22:34 +0700

    Update Prettier error

--- starter/frontend/.eslintrc.js
+++ starter/frontend/.eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
   ignorePatterns: ['build/', 'dist/', 'node_modules/'],
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'react-app',
+    'react-app/jest'
+  ],
   plugins: ['react', 'prettier'],
   settings: {
     react: {
@@ -30,7 +36,6 @@ module.exports = {
         parser: 'babel-ts',
       },
     ],
-    // Customize your rules here
     'react/prop-types': process.env.FAIL_LINT ? 2 : 0,
     'react/jsx-uses-vars': 'warn',
   },
